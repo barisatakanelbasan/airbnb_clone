1917f6af86541a949adf6ed5bfe30cf179ba6079
Barış Atakan Elbasan <barisatakanelbasan@gmail.com>
2022-10-14 17:48:46 +0300

    Map and list page updated. Draggable widget added.

---
+++ lib/product/widgets/common_widgets/search_bar_widget.dart
@@ -0,0 +1,91 @@
+import 'dart:ui';
+
+import 'package:airbnb_clone/core/extensions/context_extension.dart';
+import 'package:airbnb_clone/product/constants/product_constants.dart';
+import 'package:airbnb_clone/product/widgets/common_widgets/shimmer_widget.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import '../../../core/init/lang/locale_keys.g.dart';
+import '../../generation/assets.gen.dart';
+import '../../generation/colors.gen.dart';
+
+class SearchBarWidget extends StatefulWidget {
+  final VoidCallback onTap;
+  const SearchBarWidget({Key? key, required this.onTap}) : super(key: key);
+
+  @override
+  State<SearchBarWidget> createState() => _SearchBarWidgetState();
+}
+
+class _SearchBarWidgetState extends State<SearchBarWidget> {
+  var searchButtonTextcolor = ColorName.black;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: context.lowValue),
+      width: double.infinity,
+      height: context.height * .06,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(context.mediumValue)),
+        color: ColorName.white,
+        boxShadow: ProductConstants().defaultShadow,
+      ),
+      child: Row(
+        children: [
+          // Search Icon
+
+          ElevatedButton(
+            clipBehavior: Clip.none,
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<CircleBorder>(
+                const CircleBorder(),
+              ),
+            ),
+            onPressed: widget.onTap,
+            child: Assets.svg.icSearch.svg(
+              height: context.normalValue,
+              color: ColorName.black,
+            ),
+          ),
+
+          // Where to ?
+          ShimmerWidget(
+            enabled: false,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(LocaleKeys.search_bar_where_to.tr()),
+                Text('${LocaleKeys.search_bar_anywhere.tr()} • ${LocaleKeys.search_bar_anyweek.tr()} • ${LocaleKeys.search_bar_addguests.tr()}'),
+              ],
+            ),
+          ),
+          const Spacer(),
+
+          // Filter Button
+          Visibility(
+            visible: true, // TODO Make visible false during loading state
+            child: ElevatedButton(
+              clipBehavior: Clip.none,
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<CircleBorder>(
+                  const CircleBorder(),
+                ),
+                side: MaterialStateProperty.all<BorderSide>(
+                  BorderSide(
+                    color: ColorName.black.withOpacity(.1),
+                  ),
+                ),
+              ),
+              onPressed: widget.onTap,
+              child: Assets.svg.icFilter.svg(
+                height: context.normalValue,
+                color: ColorName.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/view/home/explore/map/explore_map_view.dart
+++ lib/view/home/explore/map/explore_map_view.dart
@@ -1,12 +1,96 @@
+import 'package:airbnb_clone/core/extensions/context_extension.dart';
+import 'package:airbnb_clone/product/constants/product_constants.dart';
+import 'package:airbnb_clone/product/generation/assets.gen.dart';
+import 'package:airbnb_clone/product/generation/colors.gen.dart';
+import 'package:airbnb_clone/product/widgets/common_widgets/search_bar_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:ui' as ui;
+import '../list/explore_list_view.dart';
 
-class ExploreMapView extends StatelessWidget {
+class ExploreMapView extends StatefulWidget {
   const ExploreMapView({Key? key}) : super(key: key);
 
   @override
+  State<ExploreMapView> createState() => _ExploreMapViewState();
+}
+
+class _ExploreMapViewState extends State<ExploreMapView> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Stack(
+            children: [
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Stack(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: (context.paddingTop + (context.highValue * 1.5)) - context.mediumValue * 2),
+                      padding: EdgeInsets.only(top: context.mediumValue * 2),
+                      width: double.infinity,
+                      child: Center(child: Text('Map')),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Container(
+                        padding: context.paddingLow,
+                        margin: EdgeInsets.only(right: context.normalValue, bottom: context.highValue * 1.1),
+                        height: context.normalValue * 2,
+                        width: context.normalValue * 2,
+                        decoration: BoxDecoration(color: ColorName.white, borderRadius: BorderRadius.circular(8), boxShadow: ProductConstants().defaultShadow),
+                        child: Assets.svg.icLocation.svg(),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  height: context.height - (context.paddingTop + (context.highValue * 1.5) + MediaQueryData.fromWindow(ui.window).padding.bottom),
+                  child: DraggableScrollableSheet(
+                    snap: true,
+                    initialChildSize: .12,
+                    minChildSize: .12,
+                    maxChildSize: 1,
+                    builder: (context, scrollController) {
+                      return ExploreListView(controller: scrollController);
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          // Appbar
+          Container(
+            padding: EdgeInsets.fromLTRB(context.mediumValue, context.paddingTop, context.mediumValue, context.mediumValue),
+            width: double.infinity,
+            height: context.paddingTop + context.highValue * 1.5,
+            decoration: BoxDecoration(
+              color: Theme.of(context).scaffoldBackgroundColor,
+              boxShadow: [
+                BoxShadow(
+                  color: ColorName.black.withOpacity(.1),
+                  offset: const Offset(0, 1),
+                  blurRadius: 3,
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                // Search Bar
+                SearchBarWidget(onTap: () {}),
+
+                // Filter Bar
+                //TODO filter bar
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
