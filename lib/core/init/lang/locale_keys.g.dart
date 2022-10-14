e04fe037b67dfd68d48a0d4679ae2433cddf5812
Barış Atakan Elbasan <barisatakanelbasan@gmail.com>
2022-10-14 17:46:08 +0300

    Assets updated

--- lib/core/init/lang/locale_keys.g.dart
+++ lib/core/init/lang/locale_keys.g.dart
@@ -8,5 +8,10 @@ abstract class  LocaleKeys {
   static const bottom_bar_buttons_inbox = 'bottom_bar_buttons.inbox';
   static const bottom_bar_buttons_profile = 'bottom_bar_buttons.profile';
   static const bottom_bar_buttons = 'bottom_bar_buttons';
+  static const search_bar_where_to = 'search_bar.where_to';
+  static const search_bar_anywhere = 'search_bar.anywhere';
+  static const search_bar_anyweek = 'search_bar.anyweek';
+  static const search_bar_addguests = 'search_bar.addguests';
+  static const search_bar = 'search_bar';
 
 }
